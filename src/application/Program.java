718618393c6e799f8c71a7c438c6f646c3825dfb
FabioBritto <fabio.tritono@gmail.com>
2024-09-10 19:20:49 -0300

    Criada a classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,27 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Department prod = new Department(1, "Livro");
-		System.out.println(prod);
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+		
+		Department department = new Department(1, "Livro");
+		System.out.println(department);
+		
+		try {
+			Seller seller = new Seller(1,"Fabio","fabio.tritono@gmail.com",sdf.parse("14/09/1996"),1250.90,department);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 }
+ 
